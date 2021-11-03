38f7c6cf093b79c60bc9e2d717ea9c81af44d9b3
root <root@localhost.localdomain>
2021-11-03 17:34:16 +0800

    视频通话第一版

--- server_chatroom.js
+++ server_chatroom.js
@@ -12,18 +12,6 @@ var serveIndex = require('serve-index');
 var socketIo = require('socket.io');
 
 
-var USERCOUNT = 3;
-
-
-
-
-
-
-
-
-
-
-
 var app = express();
 app.use(serveIndex('./public'));
 app.use(express.static('./public'));
@@ -49,24 +37,8 @@ io_sha.sockets.on('connection', (socket)=>{
 		var myRoom = io_sha.sockets.adapter.rooms[room];
 		
 		var users = Object.keys(myRoom.sockets).length;
-		
-		if(users < USERCOUNT){
 
-			socket.emit('joined', room, socket.id);	
-
-			if(users > 1){
-				
-				socket.to(room).emit('otherjoin', room);
-
-			}
-
-		}else{
-			socket.leave(room);
-			socket.emit('full', room, socket.id);
-		}
-
-
-		//io_sha.in(room).emit('joined', room, socket.id);
+		io_sha.in(room).emit('joined', room, socket.id);
 
 		//socket.broadcast.emit('joined', room, socket.id);
 
@@ -78,24 +50,20 @@ io_sha.sockets.on('connection', (socket)=>{
 		var myRoom = io_sha.sockets.adapter.rooms[room];
 		//var users = Object.keys(myRoom.sockets).length;
 		
+		socket.leave(room);
 		//io.in(room).emit('leaved', room, socket.id);
 		socket.emit('leaved', room, socket.id);
-		socket.to(room).emit('bye', room, socket.id);
 		//socket.broadcast.emit('leaved', room, socket.id);
 
 		
-		//socket.leave(room);
 	});
 
 	socket.on('message', (room, data)=>{
 		//var myroom = io.sockets.adapter.rooms[room];
 
-		//io_sha.in(room).emit('message', room, data);
+		io_sha.in(room).emit('message', room, data);
 
 		//socket.broadcast.emit('message', room, data);
-
-
-		socket.to(room).emit('message', room, data);
 
 		console.log(data);
 
