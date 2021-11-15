7b3b3a6eaa723b548881e3ec2c5f56a364e6ed9d
oldsandyoungman <874997364@qq.com>
2021-11-15 12:18:53 +0800

    以能切换的为基础，再改

--- public/pc_video_selectable2/js/main.js
+++ public/pc_video_selectable2/js/main.js
@@ -15,8 +15,312 @@ const videoSelect = document.querySelector('select#videoSource');
 const selectors = [audioInputSelect, audioOutputSelect, videoSelect];
 
 var localVideo = document.querySelector('video#localvideo');
+var remoteVideo = document.querySelector('video#remotevideo');
 
 audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
+
+var pc = null;
+
+var remoteStream = null;
+
+
+
+
+
+
+
+
+
+
+
+
+//获取url参数
+function getQueryVariable(variable)
+{
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		if(pair[0] == variable){return pair[1];}
+	}
+	return(false);
+}
+
+
+function sendMessage(roomid, data){
+
+	console.log('send message to other end', roomid, data);
+	if(!socket){
+		console.log('socket is null');
+	}
+	socket.emit('message', roomid, data);
+}
+
+function handleOfferError(err){
+	console.error('Failed to create offer:', err);
+}
+
+function handleAnswerError(err){
+	console.error('Failed to create answer:', err);
+}
+
+
+function connSignalServer(){
+
+	conn();
+
+}
+
+function conn(){
+
+	socket = io.connect();
+
+	socket.on('joined', (roomid, id) => {
+		console.log('receive joined message!', roomid, id);
+		state = 'joined'
+
+		//如果是多人的话，第一个人不该在这里创建peerConnection
+		//都等到收到一个otherjoin时再创建
+		//所以，在这个消息里应该带当前房间的用户数
+		//
+		//create conn and bind media track
+		createPeerConnection();
+		bindTracks();
+
+		btnConn.disabled = true;
+		btnLeave.disabled = false;
+		console.log('receive joined message, state=', state);
+	});
+
+	socket.on('otherjoin', (roomid) => {
+		console.log('receive joined message:', roomid, state);
+
+		//如果是多人的话，每上来一个人都要创建一个新的 peerConnection
+		//
+		if(state === 'joined_unbind'){
+			createPeerConnection();
+			bindTracks();
+		}
+
+		state = 'joined_conn';
+		call();
+
+		console.log('receive other_join message, state=', state);
+	});
+
+	socket.on('full', (roomid, id) => {
+		console.log('receive full message', roomid, id);
+		hangup();
+		closeLocalMedia();
+		state = 'leaved';
+		console.log('receive full message, state=', state);
+		alert('the room is full!');
+	});
+
+	socket.on('leaved', (roomid, id) => {
+		console.log('receive leaved message', roomid, id);
+		state='leaved'
+		socket.disconnect();
+		console.log('receive leaved message, state=', state);
+
+		btnConn.disabled = false;
+		btnLeave.disabled = true;
+	});
+
+	socket.on('bye', (room, id) => {
+		console.log('receive bye message', roomid, id);
+		//state = 'created';
+		//当是多人通话时，应该带上当前房间的用户数
+		//如果当前房间用户不小于 2, 则不用修改状态
+		//并且，关闭的应该是对应用户的peerconnection
+		//在客户端应该维护一张peerconnection表，它是
+		//一个key:value的格式，key=userid, value=peerconnection
+		state = 'joined_unbind';
+		hangup();
+		// offer.value = '';
+		// answer.value = '';
+		console.log('receive bye message, state=', state);
+	});
+
+	socket.on('disconnect', (socket) => {
+		console.log('receive disconnect message!', roomid);
+		if(!(state === 'leaved')){
+			hangup();
+			closeLocalMedia();
+
+		}
+		state = 'leaved';
+
+	});
+
+	socket.on('message', (roomid, data) => {
+		console.log('receive message!', roomid, data);
+
+		if(data === null || data === undefined){
+			console.error('the message is invalid!');
+			return;
+		}
+
+		if(data.hasOwnProperty('type') && data.type === 'offer') {
+
+			// offer.value = data.sdp;
+
+			pc.setRemoteDescription(new RTCSessionDescription(data));
+
+			//create answer
+			pc.createAnswer()
+				.then(getAnswer)
+				.catch(handleAnswerError);
+
+		}else if(data.hasOwnProperty('type') && data.type == 'answer'){
+			// answer.value = data.sdp;
+			pc.setRemoteDescription(new RTCSessionDescription(data));
+
+		}else if (data.hasOwnProperty('type') && data.type === 'candidate'){
+			var candidate = new RTCIceCandidate({
+				sdpMLineIndex: data.label,
+				candidate: data.candidate
+			});
+			pc.addIceCandidate(candidate);
+
+		}else{
+			console.log('the message is invalid!', data);
+
+		}
+
+	});
+
+
+	roomid = getQueryVariable('room');
+	socket.emit('join', roomid);
+
+	return true;
+}
+
+
+
+function createPeerConnection(){
+
+	//如果是多人的话，在这里要创建一个新的连接.
+	//新创建好的要放到一个map表中。
+	//key=userid, value=peerconnection
+	console.log('create RTCPeerConnection!');
+	if(!pc){
+		pc = new RTCPeerConnection(pcConfig);
+
+		pc.onicecandidate = (e)=>{
+
+			if(e.candidate) {
+				sendMessage(roomid, {
+					type: 'candidate',
+					label:event.candidate.sdpMLineIndex,
+					id:event.candidate.sdpMid,
+					candidate: event.candidate.candidate
+				});
+			}else{
+				console.log('this is the end candidate');
+			}
+		}
+
+		pc.ontrack = getRemoteStream;
+
+	}else {
+		console.warning('the pc have be created!');
+	}
+
+}
+
+function bindTracks(){
+
+	console.log('bind tracks into RTCPeerConnection!');
+
+	if( pc === null || pc === undefined) {
+		console.error('pc is null or undefined!');
+		return;
+	}
+
+	if(window.stream === null || window.stream === undefined) {
+		console.error('window.stream is null or undefined!');
+		return;
+	}
+
+	//add all track into peer connection
+	window.stream.getTracks().forEach((track)=>{
+		pc.addTrack(track, window.stream);
+	});
+
+}
+
+function getRemoteStream(e){
+	remoteStream = e.streams[0];
+	remoteVideo.srcObject = e.streams[0];
+}
+
+function call(){
+
+	if(state === 'joined_conn'){
+
+		var offerOptions = {
+			offerToRecieveAudio: 1,
+			offerToRecieveVideo: 1
+		}
+
+		pc.createOffer(offerOptions)
+			.then(getOffer)
+			.catch(handleOfferError);
+	}
+}
+
+function getOffer(desc){
+	pc.setLocalDescription(desc);
+	// offer.value = desc.sdp;
+	offerdesc = desc;
+
+	//send offer sdp
+	sendMessage(roomid, offerdesc);
+
+}
+
+function hangup(){
+
+	if(pc) {
+
+		offerdesc = null;
+
+		pc.close();
+		pc = null;
+	}
+
+}
+
+function closeLocalMedia(){
+
+	if(window.stream && window.stream.getTracks()){
+		window.stream.getTracks().forEach((track)=>{
+			track.stop();
+		});
+	}
+	window.stream = null;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function gotDevices(deviceInfos) {
 	// Handles being called several times to update labels. Preserve values.
@@ -110,3 +414,15 @@ audioOutputSelect.onchange = changeAudioDestination;
 videoSelect.onchange = start;
 
 start();
+
+
+
+
+
+
+
+
+
+
+
+btnConn.onclick = connSignalServer
