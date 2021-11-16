5da9f5e3948b956e854f43558b0026052c652c98
oldsandyoungman <874997364@qq.com>
2021-11-16 16:07:11 +0800

    音频输出调试，之前写的东西加上官方demo

--- public/multi2/js/main.js
+++ public/multi2/js/main.js
@@ -498,35 +498,33 @@ function refresh(){
 }
 
 function gotDevices(deviceInfos) {
-	// Handles being called several times to update labels. Preserve values.
-	const values = selectors.map(select => select.value);
-	selectors.forEach(select => {
-		while (select.firstChild) {
-			select.removeChild(select.firstChild);
-		}
-	});
+	const masterOutputSelector = document.createElement('select');
+
 	for (let i = 0; i !== deviceInfos.length; ++i) {
 		const deviceInfo = deviceInfos[i];
 		const option = document.createElement('option');
 		option.value = deviceInfo.deviceId;
-		if (deviceInfo.kind === 'audioinput') {
+		if (deviceInfo.kind === 'audiooutput') {
+			console.info('Found audio output device: ', deviceInfo.label);
+			option.text = deviceInfo.label || `speaker ${masterOutputSelector.length + 1}`;
+			masterOutputSelector.appendChild(option);
+		} else if (deviceInfo.kind === 'audioinput') {
 			option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
 			audioInputSelect.appendChild(option);
-		} else if (deviceInfo.kind === 'audiooutput') {
-			option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`;
-			audioOutputSelect.appendChild(option);
 		} else if (deviceInfo.kind === 'videoinput') {
 			option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
 			videoSelect.appendChild(option);
 		} else {
 			console.log('Some other kind of source/device: ', deviceInfo);
 		}
+
 	}
-	selectors.forEach((select, selectorIndex) => {
-		if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
-			select.value = values[selectorIndex];
-		}
-	});
+
+
+	const newOutputSelector = masterOutputSelector.cloneNode(true);
+	newOutputSelector.addEventListener('change', changeAudioDestination);
+	audioOutputSelect.parentNode.replaceChild(newOutputSelector,
+		audioOutputSelect);
 }
 
 
@@ -541,12 +539,12 @@ function change_device(){
 
 }
 
-// Attach audio output device to video element using device/sink ID.
-function attachSinkId(element, sinkId) {
+// Attach audio output device to the provided media element using the deviceId.
+function attachSinkId(element, sinkId, outputSelector) {
 	if (typeof element.sinkId !== 'undefined') {
 		element.setSinkId(sinkId)
 			.then(() => {
-				console.log(`Success, audio output device attached: ${sinkId}`);
+				console.log(`Success, audio output device attached: ${sinkId} to element with ${element.title} as source.`);
 			})
 			.catch(error => {
 				let errorMessage = error;
@@ -555,16 +553,19 @@ function attachSinkId(element, sinkId) {
 				}
 				console.error(errorMessage);
 				// Jump back to first output device in the list as it's the default.
-				audioOutputSelect.selectedIndex = 0;
+				outputSelector.selectedIndex = 0;
 			});
 	} else {
 		console.warn('Browser does not support output device selection.');
 	}
 }
 
-function changeAudioDestination() {
-	const audioDestination = audioOutputSelect.value;
-	attachSinkId(localVideo, audioDestination);
+function changeAudioDestination(event) {
+	const deviceId = event.target.value;
+	const outputSelector = event.target;
+	// FIXME: Make the media element lookup dynamic.
+	const element = event.path[2].childNodes[1];
+	attachSinkId(element, deviceId, outputSelector);
 }
 
 
