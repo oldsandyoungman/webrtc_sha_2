c150c3d725301f2ec9f4eab4d74c2d0b9e6ac2e5
oldsandyoungman <874997364@qq.com>
2021-11-16 16:49:05 +0800

    音频输出调试，之前写的东西加上官方demo

--- public/pc_video_selectable3/js/main.js
+++ public/pc_video_selectable3/js/main.js
@@ -505,11 +505,11 @@ function refresh(){
 function gotDevices(deviceInfos) {
 	// Handles being called several times to update labels. Preserve values.
 	const values = selectors.map(select => select.value);
-	selectors.forEach(select => {
-		while (select.firstChild) {
-			select.removeChild(select.firstChild);
-		}
-	});
+	// selectors.forEach(select => {
+	// 	while (select.firstChild) {
+	// 		select.removeChild(select.firstChild);
+	// 	}
+	// });
 	for (let i = 0; i !== deviceInfos.length; ++i) {
 		const deviceInfo = deviceInfos[i];
 		const option = document.createElement('option');
